7541aefe7c3868473ad7f766007c35130ef265ed
zanjeel <zanjeel123@gmail.com>
2025-02-08 18:05:46 +0000

    46

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Loader } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Leva } from "leva";
